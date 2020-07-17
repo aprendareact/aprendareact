1cbf870d52247d38fa28960bdbd936d4ad4b1d0c
Juliano Farias <thefrontendwizard@gmail.com>
2020-07-18 00:40:46 +0200

    feat(images): setup remark-images on mdx files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,12 +1,35 @@
-export const plugins = [
-  'gatsby-plugin-typescript',
-  'gatsby-plugin-postcss',
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'pages',
-      path: `${__dirname}/src/pages/`,
+module.exports = {
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
     },
-  },
-  'gatsby-plugin-mdx',
-]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
